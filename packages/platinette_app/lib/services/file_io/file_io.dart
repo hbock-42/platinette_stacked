703cf7eb0628db36ec6bee0132b17a2288af79a1
Hugo Bock <h.bock@easyrecrue.com>
2020-06-03 17:53:08 +0000

    app was crashing when no file was selected

--- packages/platinette_app/lib/services/file_io/file_io.dart
+++ packages/platinette_app/lib/services/file_io/file_io.dart
@@ -2,6 +2,14 @@ import 'dart:io';
 import 'dart:typed_data';
 
 class FileIO {
-  static Future<Uint8List> dataFromPath(String path) async =>
-      await File(path).readAsBytes();
+  static Future<Uint8List> dataFromPath(String path) async {
+    Uint8List fileData;
+    try {
+      fileData = await File(path).readAsBytes();
+    } catch (ex) {
+      print('FileIO in file_io.dart, dataFromPath, exception: $ex');
+      fileData = null;
+    }
+    return fileData;
+  }
 }
